2d79d71521b7599b7c7a0ee4408ff48480c8d94c
Christopher de Oliveira <christopherforwork@gmail.com>
2022-01-24 13:01:11 -0400

    Aplicando Estilo no Container

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,17 @@ class MyHomePage extends StatelessWidget {
                       child: Row(
                         children: [
                           Container(
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 15,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 2,
+                              ),
+                            ),
+                            padding: EdgeInsets.all(10),
                             child: Text(e.value.toString()),
                           ),
                           Column(
