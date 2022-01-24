8bee3b9ce19e5c97fb80d9a8dc1b389d541a41d9
Christopher de Oliveira <christopherforwork@gmail.com>
2022-01-24 12:40:59 -0400

    Starting new Flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      ),
+    );
+  }
+}
